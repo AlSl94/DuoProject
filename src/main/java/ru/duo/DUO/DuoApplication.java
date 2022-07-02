76cc42aec641061b437b978761156cbd588ca8e6
Alexander <71982238+AlSl94@users.noreply.github.com>
2022-07-03 00:37:35 +0300

    feat: first commit

---
+++ src/main/java/ru/duo/DUO/DuoApplication.java
@@ -0,0 +1,13 @@
+package ru.duo.DUO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuoApplication.class, args);
+	}
+
+}
