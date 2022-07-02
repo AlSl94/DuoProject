1b2d8b98756a417212a6c0bd7d5dd5a50aaaccdf
Alexander <71982238+AlSl94@users.noreply.github.com>
2022-07-03 00:42:02 +0300

    feat: test commit

--- src/main/java/ru/duo/DUO/DuoApplication.java
+++ src/main/java/ru/duo/DUO/DuoApplication.java
@@ -2,8 +2,10 @@ package ru.duo.DUO;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DuoApplication {
 
 	public static void main(String[] args) {
